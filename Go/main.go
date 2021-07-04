bb71d5542f1a90e3fe69770ad7ed56a5373c399f
Atsika <micronesic@live.fr>
2021-07-04 12:19:37 +0200

    Minor changes

--- Go/main.go
+++ Go/main.go
@@ -9,35 +9,35 @@ import (
 	"time"
 )
 
-func add(shellcode []byte, nb int) []byte {
+func add(shellcode []byte, randomByte int) []byte {
 	decoder := []byte("\xeb\x11\x5f\x31\xc9\xb1\x00\x80\x6c\x0f\xff\x00\x80\xe9\x01\x75\xf6\xeb\x05\xe8\xea\xff\xff\xff")
 	decoder[6] = byte(len(shellcode))
-	decoder[11] = byte(nb)
+	decoder[11] = byte(randomByte)
 
 	for i := range shellcode {
-		shellcode[i] += byte(nb)
+		shellcode[i] += byte(randomByte)
 	}
 	return append(decoder, shellcode...)
 }
 
-func sub(shellcode []byte, nb int) []byte {
+func sub(shellcode []byte, randomByte int) []byte {
 	decoder := []byte("\xeb\x11\x5f\x31\xc9\xb1\x00\x80\x44\x0f\xff\x00\x80\xe9\x01\x75\xf6\xeb\x05\xe8\xea\xff\xff\xff")
 	decoder[6] = byte(len(shellcode))
-	decoder[11] = byte(nb)
+	decoder[11] = byte(randomByte)
 
 	for i := range shellcode {
-		shellcode[i] -= byte(nb)
+		shellcode[i] -= byte(randomByte)
 	}
 	return append(decoder, shellcode...)
 }
 
-func xor(shellcode []byte, nb int) []byte {
+func xor(shellcode []byte, randomByte int) []byte {
 	decoder := []byte("\xeb\x11\x5f\x31\xc9\xb1\x00\x80\x74\x0f\xff\x00\x80\xe9\x01\x75\xf6\xeb\x05\xe8\xea\xff\xff\xff")
 	decoder[6] = byte(len(shellcode))
-	decoder[11] = byte(nb)
+	decoder[11] = byte(randomByte)
 
 	for i := range shellcode {
-		shellcode[i] ^= byte(nb)
+		shellcode[i] ^= byte(randomByte)
 	}
 	return append(decoder, shellcode...)
 }
@@ -45,42 +45,41 @@ func xor(shellcode []byte, nb int) []byte {
 func swap(shellcode []byte) []byte {
 	decoder := []byte("\xeb\x20\x5e\x31\xc0\x31\xdb\x31\xc9\xb1\x00\x8a\x44\x0e\xff\x8a\x5c\x0e\xfe\x88\x5c\x0e\xff\x88\x44\x0e\xfe\x80\xe9\x02\x75\xeb\xeb\x05\xe8\xdb\xff\xff\xff")
 
-	sc_len := len(shellcode)
-	if sc_len%2 != 0 {
+	shellcodeLen := len(shellcode)
+	if shellcodeLen%2 != 0 {
 		shellcode = append(shellcode, 0x90)
-		sc_len += 1
+		shellcodeLen += 1
 	}
-	decoder[10] = byte(sc_len)
-	for i := 0; i < sc_len; i += 2 {
+	decoder[10] = byte(shellcodeLen)
+	for i := 0; i < shellcodeLen; i += 2 {
 		shellcode[i], shellcode[i+1] = shellcode[i+1], shellcode[i]
 	}
 	return append(decoder, shellcode...)
 }
 
-func display_sc(shellcode []byte) {
+func printShellcode(shellcode []byte) {
 	fmt.Println("\nShellcode len:", len(shellcode))
-	fmt.Printf("\nShellcode: \n")
+	fmt.Println("\nShellcode:")
 	for i := range shellcode {
-
 		fmt.Printf("\\x%02x", shellcode[i])
 	}
 	fmt.Println()
 }
 
 func random(shellcode []byte) []byte {
-	nb := rand.Intn(255) + 1
+	randomByte := rand.Intn(255) + 1
 	method := rand.Intn(4)
 	if method == 0 {
-		fmt.Printf("[+] Add method (0x%x)\n", nb)
-		return add(shellcode, nb)
+		fmt.Printf("[+] Add method (0x%x)\n", randomByte)
+		return add(shellcode, randomByte)
 	} else if method == 1 {
-		fmt.Printf("[-] Sub method (0x%x)\n", nb)
-		return sub(shellcode, nb)
+		fmt.Printf("[-] Sub method (0x%x)\n", randomByte)
+		return sub(shellcode, randomByte)
 	} else if method == 2 {
-		fmt.Printf("[^] Xor method (0x%x)\n", nb)
-		return xor(shellcode, nb)
+		fmt.Printf("[^] Xor method (0x%x)\n", randomByte)
+		return xor(shellcode, randomByte)
 	} else if method == 3 {
-		fmt.Printf("[~] Swap method \n")
+		fmt.Printf("[~] Swap method\n")
 		return swap(shellcode)
 	}
 	return shellcode
@@ -95,22 +94,22 @@ func check(e error) {
 func crazy(shellcode []byte) []byte {
 	order := rand.Perm(4)
 	for i := range order {
-		nb := rand.Intn(255) + 1
+		randomByte := rand.Intn(255) + 1
 		if i == 0 {
-			shellcode = add(shellcode, nb)
-			fmt.Printf("[+] Add method (0x%x)\n", nb)
+			shellcode = add(shellcode, randomByte)
+			fmt.Printf("[+] Add method (0x%x)\n", randomByte)
 		}
 		if i == 1 {
-			shellcode = sub(shellcode, nb)
-			fmt.Printf("[-] Sub method (0x%x)\n", nb)
+			shellcode = sub(shellcode, randomByte)
+			fmt.Printf("[-] Sub method (0x%x)\n", randomByte)
 		}
 		if i == 2 {
-			shellcode = xor(shellcode, nb)
-			fmt.Printf("[^] Xor method (0x%x)\n", nb)
+			shellcode = xor(shellcode, randomByte)
+			fmt.Printf("[^] Xor method (0x%x)\n", randomByte)
 		}
 		if i == 3 {
 			shellcode = swap(shellcode)
-			fmt.Printf("[~] Swap method \n")
+			fmt.Printf("[~] Swap method\n")
 		}
 	}
 
@@ -127,60 +126,64 @@ func brainless(shellcode []byte, rounds uint) []byte {
 func banner() {
 	fmt.Print(`
 
-			██████╗  ██████╗ ██╗  ██╗   ██╗ ██████╗  ██████╗ 
-			██╔══██╗██╔═══██╗██║  ╚██╗ ██╔╝██╔════╝ ██╔═══██╗
-			██████╔╝██║   ██║██║   ╚████╔╝ ██║  ███╗██║   ██║
-			██╔═══╝ ██║   ██║██║    ╚██╔╝  ██║   ██║██║   ██║
-			██║     ╚██████╔╝███████╗██║   ╚██████╔╝╚██████╔╝
-			╚═╝      ╚═════╝ ╚══════╝╚═╝    ╚═════╝  ╚═════╝ 
-                  
-			
-					made with ♥ by leco & atsika
+		██████╗  ██████╗ ██╗  ██╗   ██╗ ██████╗  ██████╗ 
+		██╔══██╗██╔═══██╗██║  ╚██╗ ██╔╝██╔════╝ ██╔═══██╗
+		██████╔╝██║   ██║██║   ╚████╔╝ ██║  ███╗██║   ██║
+		██╔═══╝ ██║   ██║██║    ╚██╔╝  ██║   ██║██║   ██║
+		██║     ╚██████╔╝███████╗██║   ╚██████╔╝╚██████╔╝
+		╚═╝      ╚═════╝ ╚══════╝╚═╝    ╚═════╝  ╚═════╝ 
+                 
+		
+			  made with ♥ by leco & atsika
 
-	`)
+`)
+
 }
 
 func main() {
 
 	banner()
-	rand.Seed(time.Now().UnixNano())
-	nb := rand.Intn(255) + 1
 
-	arg_brainless := flag.Uint("brainless", 0, "Specify the number of recursive encapsulated obfuscation methods")
-	arg_crazy := flag.Bool("crazy", false, "Recursively obfuscate the shellcode with all methods")
-	arg_file := flag.String("f", "", "File with the shellcode")
-	arg_xor := flag.Bool("xor", false, "Use xor ofuscation")
-	arg_add := flag.Bool("add", false, "Use add ofuscation")
-	arg_sub := flag.Bool("sub", false, "Use sub ofuscation")
-	arg_swap := flag.Bool("swap", false, "Use swap ofuscation")
-	arg_rdm := flag.Bool("random", false, "Use random ofuscation")
+	rand.Seed(time.Now().UnixNano())
+	randomByte := rand.Intn(255) + 1
+
+	argBrainless := flag.Uint("brainless", 0, "Specify the number of recursive encapsulated obfuscation methods")
+	argCrazy := flag.Bool("crazy", false, "Recursively obfuscate the shellcode with all methods")
+	argFile := flag.String("f", "", "File containing raw shellcode")
+	argXor := flag.Bool("xor", false, "Use xor ofuscation")
+	argAdd := flag.Bool("add", false, "Use add ofuscation")
+	argSub := flag.Bool("sub", false, "Use sub ofuscation")
+	argSwap := flag.Bool("swap", false, "Use swap ofuscation")
+	argRandom := flag.Bool("random", false, "Use random ofuscation")
 
 	flag.Parse()
 
-	shellcode, err := ioutil.ReadFile(*arg_file)
+	if *argFile == "" {
+		fmt.Println("Missing -f argument. Try", os.Args[0], "-h.")
+		os.Exit(1)
+	}
+
+	shellcode, err := ioutil.ReadFile(*argFile)
 	check(err)
 
-	if *arg_file == "" {
-		fmt.Printf("Usage :\t./binary -f <path_to_file> <args>\n\ntry -h for help menu\n\n")
+	if *argAdd {
+		shellcode = add(shellcode, randomByte)
+	} else if *argSub {
+		shellcode = sub(shellcode, randomByte)
+	} else if *argXor {
+		shellcode = xor(shellcode, randomByte)
+	} else if *argSwap {
+		shellcode = swap(shellcode)
+	} else if *argRandom {
+		shellcode = random(shellcode)
+	} else if *argCrazy {
+		shellcode = crazy(shellcode)
+	} else if *argBrainless != 0 {
+		shellcode = brainless(shellcode, *argBrainless)
+	} else {
+		fmt.Println("Missing arguments. Specify an obfuscation method. Try", os.Args[0], "-h.")
 		os.Exit(1)
 	}
 
-	if *arg_add {
-		display_sc(add(shellcode, nb))
-	} else if *arg_sub {
-		display_sc(sub(shellcode, nb))
-	} else if *arg_xor {
-		display_sc(xor(shellcode, nb))
-	} else if *arg_swap {
-		display_sc(swap(shellcode))
-	} else if *arg_rdm {
-		display_sc(random(shellcode))
-	} else if *arg_crazy {
-		display_sc(crazy(shellcode))
-	} else if *arg_brainless != 0 {
-		display_sc(brainless(shellcode, *arg_brainless))
-	} else {
-		fmt.Printf("Usage :\t./binary  <args>\n\ntry -h for help menu\n\n")
-		os.Exit(1)
-	}
+	printShellcode(shellcode)
 }
